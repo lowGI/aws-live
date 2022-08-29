428f7241b7c3a3022a9054548f52cba32c4868cb
Joshie <74340767+lowGI@users.noreply.github.com>
2022-08-29 14:44:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cfbxja6pcmsd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "joshuahorshenyang-employee"
+customregion = "us-east-1"
